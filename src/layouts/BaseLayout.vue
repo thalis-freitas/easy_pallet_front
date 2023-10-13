1ab23b707faae20e4443a2bb3edba20e06912d45
Thalis Freitas <ofthalis@gmail.com>
2023-10-13 19:09:10 -0300

    add base layout component

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="card mt-4 mb-4">
+
+      <div class="card-header bg-dark text-light">
+        <slot name="title"/>
+      </div>
+
+      <div class="card-body">
+        <slot/>
+      </div>
+    </div>
+  </div>
+</template>
