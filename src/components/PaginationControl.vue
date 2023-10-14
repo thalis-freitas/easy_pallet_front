6217c85f5f9a1241898c6ff039164183cd691135
Thalis Freitas <ofthalis@gmail.com>
2023-10-14 04:47:29 -0300

    added conditional display of pagination

--- src/components/PaginationControl.vue
+++ src/components/PaginationControl.vue
@@ -46,7 +46,7 @@ const pagesToShow = computed(() => {
 </script>
 
 <template>
-  <nav aria-label="Page navigation">
+  <nav v-if="totalPage > 1" aria-label="Page navigation">
     <ul class="pagination">
       <li class="page-item" :class="{ disabled: currentPage === 1 }">
         <a
