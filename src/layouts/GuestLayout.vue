3fba70bda17d0d613b7f3e83ae71aa47128d8627
Thalis Freitas <ofthalis@gmail.com>
2023-10-15 01:48:12 -0300

    set dark background

--- src/layouts/GuestLayout.vue
+++ src/layouts/GuestLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg">
     <div class="container">
       <RouterLink class="navbar-brand" to="/">Easy Pallet</RouterLink>
     </div>
@@ -8,7 +8,7 @@
   <div class="container">
     <div class="row justify-content-center mt-5">
       <div class="col-md-6">
-        <div class="card mt-4 mb-4">
+        <div class="card mt-4 mb-4 border-2">
 
           <div class="card-header bg-dark text-light">
             <slot name="title"/>
