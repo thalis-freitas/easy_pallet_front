92c7f908765d2cd1fb9a5b02b7b4c693d4c99307
Thalis Freitas <ofthalis@gmail.com>
2023-10-15 01:42:23 -0300

    add alert component

---
+++ src/components/AlertWarning.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <div class="alert alert-warning text-center shadow border border-0">
+    {{ message }}
+  </div>
+</template>
